3f27e6ff86e868ce8fd9676b38eb1fa3eaec4f14
juanig98 <juanigalarza98@gmail.com>
2021-05-04 16:09:02 -0300

    format

--- __main__.py
+++ __main__.py
@@ -5,35 +5,26 @@ from settings import *
 if __name__ == "__main__":
 
     # Set log file
-    log = open("logs/log_"+date+".log", "w")
+    log = open("logs/log_" + date + ".log", "w")
     log.write(timestamp + "Iniciando copía\n")
     log.write(timestamp + "Fecha: " + date + "\n")
 
     try:
         client = paramiko.SSHClient()  # Declaracion de paramiko
-        client.set_missing_host_key_policy(
-            paramiko.AutoAddPolicy()
-        )  # Establecimiento de politicas de host
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Establecimiento de politicas de host
 
-        client.connect(  # Establecimiento de conexión SSH
-            conection["host"], username=conection["user_ssh"], password=conection["passwd_ssh"]
-        )
+        client.connect(conection["host"], username=conection["user_ssh"], password=conection["passwd_ssh"])  # Establecimiento de conexión SSH
         log.write(timestamp + "La conexión SSH se estableció correctamente\n")
 
         # Inicio de sesión SSH mediante Canal
         session = client.get_transport().open_session()
 
         if session.active:  # Si está activa
-            session.exec_command(
-                "chmod 777 -R /var/lib/automysqlbackup"
-            )  # Conceder permisos
-            log.write(
-                timestamp + "Se han otorgado los permisos correctamente\n")
+            session.exec_command("chmod 777 -R /var/lib/automysqlbackup")  # Conceder permisos
+            log.write(timestamp + "Se han otorgado los permisos correctamente\n")
             client.close()  # Cierre de la conexion SSH
             log.write(timestamp + "La conexión SSH ha finalizado\n")
-            client.connect(  # Establecimiento de conexión SFTP
-                conection["host"], username=conection["user_ftp"], password=conection["passwd_ftp"]
-            )
+            client.connect(conection["host"], username=conection["user_ftp"], password=conection["passwd_ftp"])  # Establecimiento de conexión SFTP
 
         # Inicio de sesión SFTP
         sftp_client = client.open_sftp()
@@ -45,7 +36,7 @@ if __name__ == "__main__":
         )
 
         log.write(timestamp + "El backup se copió existosamente\n")
-        log.write(timestamp + "Info: \n\tArchivo remoto: "+ remote_path + remote_filename + "\n\tArchivo local: " + local_path+local_filename+ "\n")
+        log.write(timestamp + "Info: \n\tArchivo remoto: " + remote_path + remote_filename + "\n\tArchivo local: " + local_path + local_filename + "\n")
         sftp_client.close()  # Cierre de sesión SFTP
         log.write(timestamp + "Sesión finalizada\n")
 
